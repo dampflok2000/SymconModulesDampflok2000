07a04735ee5346f5ec3ef00423912d9a5bb88d19
dampflok2000 <misc@peteroestreich.de>
2021-09-25 22:47:41 +0200

    Set data only is different

--- Sonnenbatterie/module.php
+++ Sonnenbatterie/module.php
@@ -11,6 +11,7 @@
             $this->RegisterTimer("TimerGetData", 0, "SOB_GetData(".$this->InstanceID.");");
 
             $this->RegisterPropertyString("IPAddress","z. B. 192.168.1.25");
+            $this->RegisterPropertyString("HostPort","8080");
             $this->RegisterPropertyInteger("IntervalUpdateTimerSeconds", 0);
         }
  
@@ -27,7 +28,7 @@
             $this->SetTimerInterval("TimerGetData", $IntervalTimerGetData);
             
 
-            //Variablen anzeigen:
+            //region Register variables:
             $this->RegisterVariableInteger("USOC", "Ladezustand");
             $this->RegisterVariableInteger("Consumption", "Gesamtverbrauch");
             $this->RegisterVariableInteger("PAC_total", "Batterieverbrauch");
@@ -43,17 +44,20 @@
             $this->RegisterVariableBoolean("FlowProductionGrid", "Stromfluss aus PV ins Stromnetz");
             $this->RegisterVariableBoolean("FlowProductionBattery", "Stromfluss aus PV in Batterie");
             $this->RegisterVariableString("SystemStatus", "Systemstatus");
+            //endregion
         }
  
-        //Modulfunktionen:
+        //region Modul Functions:
         public function GetData() {
 
-            //Get variables
+            //region Get Basic Variables
             $SOBHostAddress = $this->ReadPropertyString("IPAddress");
+            $SOBHostPort = $this->ReadPropertyString("HostPort");
             $ModulInfo = IPS_GetInstance($this->InstanceID);
             $ModulName = $ModulInfo['ModuleInfo']['ModuleName'];
+            //endregion
 
-            //Get Data
+            //region Manage Data
             $json = Sys_GetURLContent("http://".$SOBHostAddress.":8080/api/v1/status");
             if ($json == false) {
                 $this->SetStatus(230);
@@ -66,17 +70,89 @@
                 $this->SendDebug($ModulName, "Successfully got data from Sonnenbatterie!", 0);
                 $this->SendDebug($ModulName, $json, 0);
                 
+                //region Get current values
+                $Consumption_W_Id = IPS_GetObjectIDByIdent("Consumption", $this->InstanceID);
+                $Consumption_W_CurrentVal = GetValueInteger($Consumption_W_Id);
+                $Pac_total_W_Id = IPS_GetObjectIDByIdent("PAC_total", $this->InstanceID);
+                $Pac_total_W_CurrentVal = GetValueInteger($Pac_total_W_Id);
+                $GridFeedIn_W_Id = IPS_GetObjectIDByIdent("GridFeedIn", $this->InstanceID);
+                $GridFeedIn_W_CurrentVal = GetValueInteger($GridFeedIn_W_Id);
+                $Production_W_Id = IPS_GetObjectIDByIdent("Production", $this->InstanceID);
+                $Production_W_CurrentVal = GetValueInteger($Production_W_Id);
                 $USOC_Id = IPS_GetObjectIDByIdent("USOC", $this->InstanceID);
                 $USOC_CurrentVal = GetValueInteger($USOC_Id);
+                $BatteryCharging_Id = IPS_GetObjectIDByIdent("BatteryCharging", $this->InstanceID);
+                $BatteryCharging_CurrentVal = GetValueBoolean($BatteryCharging_Id);
+                $BatteryDischarging_Id = IPS_GetObjectIDByIdent("BatteryDischarging", $this->InstanceID);
+                $BatteryDischarging_CurrentVal = GetValueBoolean($BatteryDischarging_Id);
+                $FlowConsumptionBattery_Id = IPS_GetObjectIDByIdent("FlowConsumptionBattery", $this->InstanceID);
+                $FlowConsumptionBattery_CurrentVal = GetValueBoolean($FlowConsumptionBattery_Id);
+                $FlowConsumptionGrid_Id = IPS_GetObjectIDByIdent("FlowConsumptionGrid", $this->InstanceID);
+                $FlowConsumptionGrid_CurrentVal = GetValueBoolean($FlowConsumptionGrid_Id);
+                $FlowConsumptionProduction_Id = IPS_GetObjectIDByIdent("FlowConsumptionProduction", $this->InstanceID);
+                $FlowConsumptionProduction_CurrentVal = GetValueBoolean($FlowConsumptionProduction_Id);
+                $FlowGridBattery_Id = IPS_GetObjectIDByIdent("FlowGridBattery", $this->InstanceID);
+                $FlowGridBattery_CurrentVal = GetValueBoolean($FlowGridBattery_Id);
+                $FlowProductionBattery_Id = IPS_GetObjectIDByIdent("FlowProductionBattery", $this->InstanceID);
+                $FlowProductionBattery_CurrentVal = GetValueBoolean($FlowProductionBattery_Id);
+                $FlowProductionGrid_Id = IPS_GetObjectIDByIdent("FlowProductionGrid", $this->InstanceID);
+                $FlowProductionGrid_CurrentVal = GetValueBoolean($FlowProductionGrid_Id);
+                $SystemStatus_Id = IPS_GetObjectIDByIdent("SystemStatus", $this->InstanceID);
+                $SystemStatus_CurrentVal = GetValueString($SystemStatus_Id);
+                //endregion
+
+                //region Check if value is different
+                If ($Consumption_W_CurrentVal <> $objData->Consumption_W) {
+                    SetValueInteger($Consumption_W_Id,intval($objData->Consumption_W));
+                }
+                If ($PACTotal_CurrentVal <> $objData->Pac_total_W) {
+                    SetValueInteger($PACTotal_Id,intval($objData->Pac_total_W));
+                }
+                If ($GridFeedIn_W_CurrentVal <> $objData->GridFeedIn_W) {
+                    SetValueInteger($GridFeedIn_W_Id,intval($objData->GridFeedIn_W));
+                }
+                If ($Production_W_CurrentVal <> $objData->Production_W) {
+                    SetValueInteger($Production_W_Id,intval($objData->Production_W));
+                }
                 If ($USOC_CurrentVal <> $objData->USOC) {
                     SetValueInteger($USOC_Id,intval($objData->USOC));
                 }
-
+                If ($BatteryCharging_CurrentVal <> $objData->BatteryCharging) {
+                    SetValueBoolean($BatteryCharging_Id,intval($objData->BatteryCharging));
+                }
+                If ($BatteryDischarging_CurrentVal <> $objData->BatteryDischarging) {
+                    SetValueBoolean($BatteryDischarging_Id,intval($objData->BatteryDischarging));
+                }
+                If ($FlowConsumptionBattery_CurrentVal <> $objData->FlowConsumptionBattery) {
+                    SetValueBoolean($FlowConsumptionBattery_Id,intval($objData->FlowConsumptionBattery));
+                }
+                If ($FlowConsumptionGrid_CurrentVal <> $objData->FlowConsumptionGrid) {
+                    SetValueBoolean($FlowConsumptionGrid_Id,intval($objData->FlowConsumptionGrid));
+                }
+                If ($FlowConsumptionProduction_CurrentVal <> $objData->FlowConsumptionProduction) {
+                    SetValueBoolean($FlowConsumptionProduction_Id,intval($objData->FlowConsumptionProduction));
+                }
+                If ($FlowGridBattery_CurrentVal <> $objData->FlowGridBattery) {
+                    SetValueBoolean($FlowGridBattery_Id,intval($objData->FlowGridBattery));
+                }
+                If ($FlowProductionBattery_CurrentVal <> $objData->FlowProductionBattery) {
+                    SetValueBoolean($FlowProductionBattery_Id,intval($objData->FlowProductionBattery));
+                }
+                If ($FlowProductionGrid_CurrentVal <> $objData->FlowProductionGrid) {
+                    SetValueBoolean($FlowProductionGrid_Id,intval($objData->FlowProductionGrid));
+                }
+                If ($SystemStatus_CurrentVal <> $objData->SystemStatus) {
+                    SetValueString($SystemStatus_Id,intval($objData->SystemStatus));
+                }
+                //endregion
+                
+                //region Old commands
+                /*
                 SetValueInteger(IPS_GetObjectIDByIdent("Consumption", $this->InstanceID),intval($objData->Consumption_W));
                 SetValueInteger(IPS_GetObjectIDByIdent("PAC_total", $this->InstanceID),intval($objData->Pac_total_W));
                 SetValueInteger(IPS_GetObjectIDByIdent("GridFeedIn", $this->InstanceID),intval($objData->GridFeedIn_W));
                 SetValueInteger(IPS_GetObjectIDByIdent("Production", $this->InstanceID),intval($objData->Production_W));
-                //SetValueInteger(IPS_GetObjectIDByIdent("USOC", $this->InstanceID),intval($objData->USOC));
+                SetValueInteger(IPS_GetObjectIDByIdent("USOC", $this->InstanceID),intval($objData->USOC));
                 SetValueBoolean(IPS_GetObjectIDByIdent("BatteryCharging", $this->InstanceID),boolval($objData->BatteryCharging));
                 SetValueBoolean(IPS_GetObjectIDByIdent("BatteryDischarging", $this->InstanceID),boolval($objData->BatteryDischarging));
                 SetValueBoolean(IPS_GetObjectIDByIdent("FlowConsumptionBattery", $this->InstanceID),boolval($objData->FlowConsumptionBattery));
@@ -86,8 +162,11 @@
                 SetValueBoolean(IPS_GetObjectIDByIdent("FlowProductionBattery", $this->InstanceID),boolval($objData->FlowProductionBattery));
                 SetValueBoolean(IPS_GetObjectIDByIdent("FlowProductionGrid", $this->InstanceID),boolval($objData->FlowProductionGrid));
                 SetValueString(IPS_GetObjectIDByIdent("SystemStatus", $this->InstanceID),$objData->SystemStatus);
-
+                */
+                //endregion
             }
+            //endregion
         }
+        //endregion
     }
     ?>
